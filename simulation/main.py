bab1eb5fe5a3406e1ba0c8f014ef853e73069ee9
srijit-m <srijit.mukherjee@uqconnect.edu.au>
2026-01-15 23:55:55 +1000

    Have managed to implement floodfill number mapping for no walls. Fixed an index error by making sure that neighbour coordinates were not out of bounds e.g. for a point like 0,2 its northern neighbour does not exist. Code is still fragile so need to improve some things to make it more robust

--- simulation/main.py
+++ simulation/main.py
@@ -9,13 +9,18 @@ COLUMN_NUM = 9
 
 def get_neighbour_coords(row, col):
         """This function returns the north, east, south and west coordinates of the points relative
-        to a point popped out of the queue.
+        to a point popped out of the queue. For neighbour points that are out of bounds, they will be set to none
         Returns: A list of 4 tuples of the form (row, col)"""
         neighbour_points = []
         neighbour_points.append((row+Direction.NORTH.value[0], col+Direction.NORTH.value[1]))
         neighbour_points.append((row+Direction.EAST.value[0], col+Direction.EAST.value[1]))
         neighbour_points.append((row+Direction.SOUTH.value[0], col+Direction.SOUTH.value[1]))
         neighbour_points.append((row+Direction.WEST.value[0], col+Direction.WEST.value[1]))
+        for i in range(4):
+            if neighbour_points[i][0] < 0 or neighbour_points[i][0] > 8:
+                neighbour_points[i] = None
+            elif neighbour_points[i][1] < 0 or neighbour_points[i][1] > 8:
+                neighbour_points[i] = None
         return neighbour_points
 class Direction(Enum):
     NORTH = (-1, 0)
@@ -76,45 +81,40 @@ class Maze:
     def calculate_floodfill_distances(self, row_num, col_num, dq, floodfill_distances, hor_walls, vert_walls):
         #Add the goal cell to the queue
         centre_point = (row_num//2, col_num//2)
+        #print(f"Row: {centre_point[0]}, Column: {centre_point[1]}")
         dq.append(centre_point)
         while dq:
             #Carry out the floodfill algorithm
             #Pop the zero cell and save its row and column
-            (row, col) = dq.pop()
-            print(f"Row: {row}, Col: {col}")
+            (row, col) = dq.popleft()
             #Now check the boxes north, east, south and west of the point
             neighbour_points = get_neighbour_coords(row, col)
             #Note that in this for loop False + 1 = 1 (cool thing in python)
             for i in range(4):
-                if (i == 0):
-                    #Looking at the north cell so thus horizontal walls
-                    if (hor_walls[row][col] == False):
-                        floodfill_distances[neighbour_points[i][0]][neighbour_points[i][1]] = floodfill_distances[row][col] +1
-                        #Add cell to queue
-                        dq.append(neighbour_points[i])
-                elif (i == 1):
-                    #Looking at the east cell so thus vertical walls
-                    if (vert_walls[row][col+1] == False):
-                        #There are no walls
-                        floodfill_distances[neighbour_points[i][0]][neighbour_points[i][1]] = floodfill_distances[row][col] +1
-                        dq.append(neighbour_points[i])
-                elif (i == 2):
-                    #Looking at the south cell, so horizontal walls
-                    if (hor_walls[row+1][col] == False):
-                        floodfill_distances[neighbour_points[i][0]][neighbour_points[i][1]] = floodfill_distances[row][col] +1
-                        dq.append(neighbour_points[i])
-                elif (i == 3):
-                    #Looking at the west cell, so thus vertical walls
-                    if (vert_walls[row][col] == False):
-                        floodfill_distances[neighbour_points[i][0]][neighbour_points[i][1]] = floodfill_distances[row][col] +1
-                        dq.append(neighbour_points[i])
-
-
-
-
-                    
-    
-        
+                #First check if the neighbour cell is unvisited
+                if (neighbour_points[i] != None) and (floodfill_distances[neighbour_points[i][0]][neighbour_points[i][1]] == False):
+                    if (i == 0):
+                        #Looking at the north cell so thus horizontal walls
+                        if hor_walls[row][col] == False:
+                            floodfill_distances[neighbour_points[i][0]][neighbour_points[i][1]] = floodfill_distances[row][col] +1
+                            #Add cell to queue
+                            dq.append(neighbour_points[i])
+                    elif (i == 1):
+                        #Looking at the east cell so thus vertical walls
+                        if vert_walls[row][col+1] == False:
+                            #There are no walls
+                            floodfill_distances[neighbour_points[i][0]][neighbour_points[i][1]] = floodfill_distances[row][col] +1
+                            dq.append(neighbour_points[i])
+                    elif (i == 2):
+                        #Looking at the south cell, so horizontal walls
+                        if hor_walls[row+1][col] == False:
+                            floodfill_distances[neighbour_points[i][0]][neighbour_points[i][1]] = floodfill_distances[row][col] +1
+                            dq.append(neighbour_points[i])
+                    elif (i == 3):
+                        #Looking at the west cell, so thus vertical walls
+                        if vert_walls[row][col] == False:
+                            floodfill_distances[neighbour_points[i][0]][neighbour_points[i][1]] = floodfill_distances[row][col] +1
+                            dq.append(neighbour_points[i])
 
 
 
@@ -131,7 +131,11 @@ def main():
     maze = Maze(ROW_NUM, COLUMN_NUM)
     maze.initialise_wall_variables(maze.hor_walls, maze.vert_walls)
     maze.initialise_floodfill_nums(maze.floodfill_distances, maze._row_num, maze._col_num)
-
+    maze.calculate_floodfill_distances(maze._row_num, maze._col_num, maze.dq, maze.floodfill_distances, maze.hor_walls, maze.vert_walls)
+    print(maze.floodfill_distances[1][2])
+    print(maze.floodfill_distances[2][1])
+    print(maze.floodfill_distances[3][1])
+    print(maze.floodfill_distances[2][3])
 
     #Wall follower algorithm    
     while True:
